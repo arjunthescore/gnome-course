b17cbe89070a69aac9b752b8b0abcb8cf0572ee6
Arjun <arjun.bhandal@thescore.com>
2024-01-14 14:38:20 -0500

    Text client completed

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -1,6 +1,6 @@
 defmodule TextClient.Impl.Player do
-  @typep game :: Hangman.game
-  @typep tally :: Hangman.tally
+  @typep game :: Hangman.game()
+  @typep tally :: Hangman.tally()
   @typep state :: {game, tally}
 
   @spec start() :: :ok
@@ -9,38 +9,58 @@ defmodule TextClient.Impl.Player do
     tally = Hangman.tally(game)
     interact({game, tally})
   end
-  #@type state :: :initializing | :won | :good_guess | :bad_guess | :already_use
+
+  # @type state :: :initializing | :won | :good_guess | :bad_guess | :already_use
   @spec interact(state) :: :ok
 
   def interact({_game, _tally = %{game_state: :won}}) do
-    IO.puts "Congratulations, you've won!!!"
+    IO.puts("Congratulations, you've won!!!")
   end
 
-  def interact({_game, tally = %{game_state: :lost}}) do
-    IO.puts("Better luck next time, you've lost. Correct answer: #{tally.letters |> Enum.join()}")
+  def interact({game, _tally = %{game_state: :lost}}) do
+    IO.puts("Better luck next time, you've lost. Word: #{game.letters |> Enum.join()}")
   end
 
   def interact({game, tally}) do
-    IO.puts feedback_for(tally)
-    IO.puts tally.letters |> Enum.join()
-    guess = IO.gets("Please enter your next guess:\n")
-    Hangman.make_move(game, guess)
+    IO.puts(feedback_for(tally))
+    IO.puts(current_word(tally))
+
+    Hangman.make_move(game, get_guess())
     |> interact()
   end
 
-  #@type state :: :initializing | :won | :good_guess | :bad_guess | :already_use
+  # @type state :: :initializing | :won | :good_guess | :bad_guess | :already_use
 
-
-  defp feedback_for(tally = %{game: :intialized}) do
-    "I'm thinking of a #{tally.letters |> length} word"
+  defp feedback_for(tally = %{game_state: :initializing}) do
+    "I'm thinking of a #{tally.letters |> length} letter word"
   end
-  defp feedback_for(_tally = %{game: :good_guess}) do
+
+  defp feedback_for(_tally = %{game_state: :good_guess}) do
     "Good guess!"
   end
-  defp feedback_for(_tally = %{game: :bad_guess}) do
+
+  defp feedback_for(_tally = %{game_state: :bad_guess}) do
     "Sorry that letter is not in the word"
   end
-  defp feedback_for(_tally = %{game: :already_use}) do
+
+  defp feedback_for(_tally = %{game_state: :already_use}) do
     "Sorry you've already used that letter"
+  end
+
+  defp get_guess() do
+    IO.gets("Next letter: ")
+    |> String.trim()
+    |> String.downcase()
+  end
+
+  def current_word(tally) do
+    [
+      "Word, so far:",
+      tally.letters |> Enum.join(" "),
+      "   turns left: ",
+      tally.turns_left |> to_string,
+      "   used so far: ",
+      tally.used |> Enum.join(",")
+    ]
   end
 end
